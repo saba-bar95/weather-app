8c9df8748e63042ce64223af79554c2e88fbc8aa
saba-bar95 <s.barbakadze95@gmail.com>
2024-01-06 14:53:57 +0400

    change http to https

--- src/components/logic/getData.js
+++ src/components/logic/getData.js
@@ -1,7 +1,7 @@
 export default async function getData(val) {
   try {
     const location = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=02acb9e290594790a2995550231412&q=${val}&aqi=no`
+      `https://api.weatherapi.com/v1/current.json?key=02acb9e290594790a2995550231412&q=${val}&aqi=no`
     );
     const parsedLocation = await location.json();
     return parsedLocation;
